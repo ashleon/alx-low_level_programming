9b89d81969fa5bad74e45700ee296d1b6b4e84fe
ashleon <felixleonmax@gmail.com>
2022-12-15 07:25:15 -0800

    returns numbers upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,20 +12,20 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while(n <= 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
+		printf("%d", n);
 	}
 	else if (n > 98)
 	{
-		do
-		{
+		do{
 			printf("%d, ", n);
 			n--;
-		}while (n >= 98);
+		} while (n >= 98);
 	}
 	else
-		printf("%d, ", n);
+		printf("%d", n);
 }
