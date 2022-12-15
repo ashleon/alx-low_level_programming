e56332040651e8a24ecc2498397dd9b95042b252
ashleon <felixleonmax@gmail.com>
2022-12-15 00:00:37 -0800

    checks if a letter is lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,12 @@
 /**
  * _islower - checks whether ascii value of a given character is within the range of lowercase letters
  *
+ * @c is the ascii value for the alphabet
  * Returns an int value
  */
 int _islower(int c)
 {
-	int islower=1,notLower=0;
+	int islower = 1 , notLower = 0;
 
 	if (c <= 122 && c >= 97)
 		return (islower);
