40bf04f9b5964d305007d2db85dbbeef5b21b964
ashleon <felixleonmax@gmail.com>
2022-12-14 22:57:03 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "ashleon.h"
+/**
+ * prints alphabets
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+	for ( c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/ashleon.h
@@ -0,0 +1,4 @@
+int _putchar(char r);
+void alphabets(void);
+void alphabets_x10(void);
+
