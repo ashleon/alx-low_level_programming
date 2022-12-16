de1e59d75c302ad0a5aa1fc714d9f42e349af5f9
ashleon <felixleonmax@gmail.com>
2022-12-16 04:57:22 -0800

    positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,9 +9,9 @@
 void positive_or_negative(int i)
 {
 	if (i < 0)
-		_putchar(-1);
+		printf("%d",-1);
 	else if ( i == 0)
-		_putchar(0);
+		printf("d",0);
 	else if (i > 0)
-		_putchar(1);
+		printf("%d",1);
 }
