cd187cc9af4cb7c5c048f9b081252a32c24dccd6
ashleon <felixleonmax@gmail.com>
2022-12-17 00:16:59 -0800

    prints 0 through 9 except for 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ * print_most_numbers - prints 0 through 9 except for 2 and 4
+ *
+ * Return: null
+ */
+void print_most_numbers(void)
+{
+	int i;
+	
+	i = 0;
+
+	do {
+		if (i == 2 || i == 4)
+		{
+		}
+		else
+			_putchar(i + '0');
+		i++;
+	} while (i <= 9);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
 int _isdigit(int);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
+
