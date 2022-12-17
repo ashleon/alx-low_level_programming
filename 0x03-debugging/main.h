7b4e1698b607a3972a58b1338079a7aa05568c7e
ashleon <felixleonmax@gmail.com>
2022-12-16 19:07:48 -0800

    positive or negative

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main.h
+#define main.h
 
 #include <stdio.h>
 
@@ -7,4 +7,5 @@ void positive_or_negative(int);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
-#endif /* MAIN_H */
+
+#endif /* main.h */
