01dc9e0c37b3f0432d114a3b9533ef0b8622cc9c
ashleon <felixleonmax@gmail.com>
2022-12-17 00:58:11 -0800

    prints a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_line - print lines
+ *
+ * @n: width of the line
+ *
+ * Return: null
+ */
+void print_line(int n)
+{
+	int m;
+
+	m = 0;
+
+	if (m < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (m; m < = n; m++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+
+}
