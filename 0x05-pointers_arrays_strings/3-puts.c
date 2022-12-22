e449f00d2f1b2f0c2fad0376d350d29d4af5f899
ashleon <felixleonmax@gmail.com>
2022-12-21 21:23:36 -0800

    prints a string, followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * _puts - Prints a string
- *
  * @str: The string to print
- *
  * Return: null
  */
 void _puts(char *str)
 {
+
 	for (; *str != '\0'; str++)
 	{
 		_putchar(*str);
 	}
 	_putchar('\n');
+}
