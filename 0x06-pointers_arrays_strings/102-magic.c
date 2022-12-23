6ed3d97b3810ede4ebb1b7d15c8ca44751127e3b
ashleon <felixleonmax@gmail.com>
2022-12-23 02:08:34 -0800

    ease

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _main- pointer function with 3 argument
+ *
+ * Return: string
+ */
+int main(void)
+{
+	int n, a[5], *p;
+
+	a[2] = 1024;
+	p = &n;
+
+
+	p[5] = 98;
+
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
